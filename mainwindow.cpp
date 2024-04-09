9167aa13c78057b9ba6e0741be07b7ed910a38b3
1915419156 <1915419156@qq.com>
2024-04-09 09:53:12 +0800

    增加图标

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -12,11 +12,13 @@ MainWindow::MainWindow(QWidget *parent):
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
     //在MainWindow 的构造函数中
     //调用了setWindowTitle()，设置主窗口的标题,文本使用tr()函数，这是一个用于 Qt 国际化的函数
     setWindowTitle(tr("Main Window"));
+
     //在堆上创建了openAction对象,在QAction构造函数，我们传入了一个图标、一个文本和 this 指针
-    openAction = new QAction(QIcon(":/images/doc-open"), tr("&Open..."), this);
+    openAction = new QAction(QIcon(":/images/document-open"), tr("&Open..."), this);
     openAction->setShortcuts(QKeySequence::Open);
     openAction->setStatusTip(tr("Open an existing file"));
     connect(openAction, &QAction::triggered, this, &MainWindow::open);
